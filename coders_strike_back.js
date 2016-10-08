37cb7c43d8b6ece56bd998dfdac3b17096fc23bc
Ismael <ismaproco@gmail.com>
2016-10-08 19:59:41 +0200

    CSB: League 3 - R 3364/4387

--- coders_strike_back.js
+++ coders_strike_back.js
@@ -1,3 +1,56 @@
+/* Vector2 - implementation
+*/
+
+// Simple Vector Class
+var Vec2 = function(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+function initVector2Props() {
+    
+    Vec2.prototype.init = function(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    Vec2.prototype.add = function(v) {
+        return new Vec2(this.x + v.x, this.y + v.y);
+    }
+
+    Vec2.prototype.subtract = function(v) {
+        return new Vec2(this.x - v.x, this.y - v.y);
+    }
+
+    Vec2.prototype.scale = function(v) {
+        return new Vec2(this.x * v, this.y * v);
+    }
+
+    Vec2.prototype.length = function() {
+        return Math.sqrt((this.x * this.x) + (this.y * this.y));
+    }
+
+    Vec2.prototype.normalize = function() {
+        var iLen = 1 / this.length();
+        return new Vec2(this.x * iLen, this.y * iLen);
+    }
+
+    Vec2.prototype.toString = function() {
+        return "x: " + this.x + " , y: " + this.y;
+    }
+
+    Vec2.prototype.equal = function(v) {
+        if( v ){
+            return this.x === v.x && this.y == v.y;    
+        }
+
+        return false;
+    }
+}
+
+initVector2Props();
+
+
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -9,13 +62,68 @@ var boost = 1;
 var avgSpeed = 0;
 var arrDistances = [];
 
-function calculateAvgSpeed( arr, points ) {
-    if( arr.length > points ) {
-        
+
+var boostedSegment=false, segmentChange=false, prevDistance = 0;
+
+
+// arrival break
+function arrivalBreak( position, distance, slowingRadius, maxVelocity ){
+    var desired_velocity;
+
+
+    if( distance < slowingRadius ) {
+        desired_velocity = position.normalize().scale(  maxVelocity * ( distance / slowingRadius ) );
+    } else {
+        desired_velocity = position.normalize().scale(  maxVelocity );
+    }
+
+    return desired_velocity;
+}
+
+// steer
+
+function outSteer( position, prevdistance, steeringRadius, maxVelocity, angle ){
+    var desired_velocity;
+    
+    if( Math.abs( angle ) < 1 ) {
+        desired_velocity = position.normalize().scale(  maxVelocity );
+    } else {
+        printErr('error: ' +prevdistance + ' rad: '+ steeringRadius )
+        desired_velocity = position.normalize().scale(  maxVelocity * ( prevdistance / steeringRadius ) );
+    }
+
+    return desired_velocity;
+}
+
+//
+
+var sections = {
+    steer: 0,
+    keep: 1,
+    break: 2
+}
+
+function sectionManager(position, prevCheck, nextCheck, prevMaxDistance, nextMaxDistance ) {
+    printErr('manager: ' +prevCheck + ' '+ position.subtract(prevCheck).length() + ' ' + prevMaxDistance );
+    if(position.subtract(prevCheck).length() < prevMaxDistance) {
+        return sections.steer;
+    }else if( nextCheck.subtract( position ).length() < nextMaxDistance ) {
+        return sections.break;
+    } else {
+        return sections.keep;
     }
 }
 
-var boostedSegment=false, segmentChange=false, prevDistance = 0;
+
+//
+var vnchk = new Vec2(); // vector next checkpoint
+var v0v1 = new Vec2(); // vector next checkpoint
+var vv1 = new Vec2(); // vector next checkpoint
+
+var fv,currentchk = new Vec2(0,0),prevchk = new Vec2(0,0);
+
+var angles = [];
+var boost = 1, boostedSegment = false;
 
 
 // game loop
@@ -31,7 +139,72 @@ while (true) {
     var opponentX = parseInt(inputs[0]);
     var opponentY = parseInt(inputs[1]);
 
-    // Write an action using print()
+    segmentChange = false;
+
+    var thrust = 100, finalx = nextCheckpointX,  finaly = nextCheckpointY;
+    
+    vnchk.init( nextCheckpointX, nextCheckpointY );
+
+    v0v1.init( x, y );
+    vv1 = vnchk.subtract( v0v1 );
+    
+    if( !vnchk.equal( currentchk ) && segmentChange == false ) {
+        printErr('SegChange: ' + segmentChange + ' ' + prevchk);
+        segmentChange = true;
+        angles.push( nextCheckpointAngle );
+
+        prevchk.x = currentchk.x;
+        prevchk.y = currentchk.y;
+
+        currentchk.x = vnchk.x;
+        currentchk.y = vnchk.y;
+    }
+
+    var section = sectionManager(v0v1, prevchk, vnchk, 1000, 2500);
+
+
+    if( section == sections.steer ) {
+        fv = outSteer( vv1, v0v1.subtract( prevchk ).length(), 1000, 100, nextCheckpointAngle);
+        segmentChange = false;    
+    } else if( section == sections.break ){
+        fv = arrivalBreak(vv1, vv1.length(), 2500, 100);
+    } else {
+        fv = vv1.normalize().scale( 100 );
+    }
+
+    //printErr('error ' + nextCheckpointDist + ' ' + nextCheckpointAngle + ' ' + thrust + ' b:'+boost + ' ' + avgSpeed + ' ' + segmentChange  );
+    //printErr('x,y ' + x +',' + y +  ' opponent:' + opponentX + ',' + opponentY  );
+
+
+
+    printErr('Vector vnchk 1: ' + vnchk + ' magnitude: ' + parseInt(vnchk.length()) );
+    printErr('Vector v0v1 1: ' + v0v1 + ' magnitude: ' +  parseInt( v0v1.length() ) );
+    printErr('Vector vehicle 1: ' + vv1 + ' magnitude: ' +  parseInt( vv1.length() ) );
+    printErr('===? nangle: ' + nextCheckpointAngle + ' mag: ' +  parseInt( fv.length() ) + ' sec:'+ section + ' ' + angles );
+    
+    thrust = parseInt(fv.length());
+    thrust = thrust > 100 ? 99 : thrust;
+
+    // boost logic
+    if(boost > 0 ) {
+        if( nextCheckpointDist > 7500 && ( nextCheckpointAngle == 0 ) ) {
+            printErr('burst ' + nextCheckpointDist + ' ' + nextCheckpointAngle + ' ' + thrust + ' b:'+boost);
+            thrust = 'BOOST';
+            boost -=1;
+            boostedSegment = true;
+        }
+    }
+
+
+    print(finalx + ' ' + finaly +' ' + thrust );
+}
+
+
+
+/*
+
+
+// Write an action using print()
     // To debug: printErr('Debug messages...');
 
     
@@ -59,13 +232,19 @@ while (true) {
     }
     
     
-    if( nextCheckpointAngle > 90 || nextCheckpointAngle < -90) {
+    if( nextCheckpointAngle > 90 || nextCheckpointAngle < -90 ) {
         if( nextCheckpointDist > 6000) {
-            thrust = 80;
+            thrust = incrementWithLimit( thrust , 0.4 , 80 );
         }else {
             thrust = 10;    
         }
-    } 
+    } else if( nextCheckpointAngle > 10 || nextCheckpointAngle < -10 ){
+
+        if( Math.abs( avgSpeed ) > 500 ) {
+            thrust = 60;
+        }
+        thrust = incrementWithLimit( thrust , 0.3 , 80 );
+    }
     
     // // booster correction
     if( nextCheckpointDist < 4000 && avgSpeed > 600 && ( nextCheckpointAngle < -5 || nextCheckpointAngle > 5 ) ) {
@@ -103,7 +282,6 @@ while (true) {
             boostedSegment = true;
         }
     }
-    printErr('error ' + nextCheckpointDist + ' ' + nextCheckpointAngle + ' ' + thrust + ' b:'+boost + ' ' + avgSpeed + ' ' + segmentChange  );
-    printErr('x,y ' + x +',' + y +  ' opponent:' + opponentX + ',' + opponentY  );
-    print(finalx + ' ' + finaly +' ' + thrust );
-}
+
+
+*/
